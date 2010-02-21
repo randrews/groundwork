c077120adcff8b3979ff55987a6e26e59eaceff1
Ross Andrews <randrews@geekfu.org>
2010-02-21 00:12:38 -0600

    Added some more functions to directory

--- kickstart.rb
+++ kickstart.rb
@@ -1,5 +1,8 @@
 module Kickstart
-  def directory name
-    FileUtils.mkdir File.join(name)
+  def directory *name, &block
+    FileUtils.mkdir_p File.join(name)
+    if block_given?
+      FileUtils.cd File.join(name), &block
+    end
   end
 end
--- test/dirs.rb
+++ test/dirs.rb
@@ -4,14 +4,14 @@ require "fileutils"
 describe "Directories" do
   include Kickstart
 
-  before :all do
+  before :each do
     @pwd = FileUtils.pwd
     @scratch = File.join(File.dirname(__FILE__),"scratch")
     FileUtils.mkdir @scratch
     FileUtils.cd @scratch
   end
 
-  after :all do
+  after :each do
     FileUtils.cd @pwd
     FileUtils.rm_rf @scratch
   end
@@ -20,5 +20,27 @@ describe "Directories" do
     directory "foo"
 
     File.directory?("foo").should be_true
+  end
+
+  it "should accept a block" do
+    x = false
+    directory "foo" do
+      x = true
+    end
+    x.should be_true
+  end
+
+  it "should create subdirectories" do
+    directory "foo" do
+      directory "bar"
+    end
+
+    File.directory?("foo/bar").should be_true
+  end
+
+  it "should make a tree of directories in one call" do
+    directory "foo", "bar"
+
+    File.directory?("foo/bar").should be_true
   end
 end
