866bb196a67cd75ca030a5a0c8d94d53dfdc4543
Ross Andrews <randrews@geekfu.org>
2010-02-22 22:29:38 -0600

    Added tar wrapper class

---
+++ tar_wrapper.rb
@@ -0,0 +1,33 @@
+require "rubygems"
+require "archive/tar/minitar"
+require "base64"
+
+class TarWrapper
+  include Archive::Tar
+
+  # Takes a base64-encoded, tarred string
+  def initialize encoded
+    tar = Base64.decode64(encoded)
+    @files = {}
+    Minitar::Reader.new(StringIO.new(tar)).each do |entry|
+      next unless entry.file?
+      @files[entry.full_name] = entry.read
+    end
+  end
+
+  def [] name
+    @files[name]
+  end
+
+  def self.compress files
+    string = StringIO.new
+    output = Minitar::Output.new(string)
+
+    files.each do |file|
+      Minitar.pack_file(file, output)
+    end
+
+    output.close
+    Base64.encode64 string.string
+  end
+end
