5a32219d63851c0469e9ddc5a77b08ea9bafc2b0
Ross Andrews <randrews@geekfu.org>
2010-02-22 23:31:38 -0600

    Added possible method

--- test/recipe.rb
+++ test/recipe.rb
@@ -18,15 +18,23 @@ describe "Recipes" do
   it "should require the proper list of files" do
     File.open("foo","w"){|f| f.print "foo" }
     File.open("bar","w"){|f| f.print "bar" }
+    File.open("baz","w"){|f| f.print "baz" }
 
     files = Kickstart.required_files do
+      possible "baz"
+
       file "one", :from=>"foo"
 
       directory "blah" do
         file "two", :from_erb=>"bar"
       end
+
+      if false
+        file "three", :from=>"fake"
+        file "four", :from=>"baz"
+      end
     end
 
-    files.should==["foo", "bar"]
+    files.sort.should==["foo", "bar", "baz"].sort
   end
 end
