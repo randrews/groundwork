f6bbe893de9746e6e7c361b789ad0d5c6a2d209f
Ross Andrews <randrews@geekfu.org>
2010-02-24 23:14:52 -0600

    Able to list recipes

--- lib/groundwork.rb
+++ lib/groundwork.rb
@@ -173,4 +173,22 @@ module Groundwork
       end
     end
   end
+
+  # Return a hash of all known recipes, the included ones and the ones in .groundwork
+  def self.known_recipes
+    recipes = {}
+    Dir[File.join(File.dirname(__FILE__),"..","recipes","*.recipe")].each do |recipe|
+      recipes[File.basename(recipe,".recipe")] = recipe
+    end
+
+    dot_groundwork = File.expand_path("~/.groundwork")
+
+    if File.directory?(dot_groundwork)
+      Dir[File.join(dot_groundwork,"*.recipe")].each do |recipe|
+        recipes[File.basename(recipe,".recipe")] = recipe
+      end
+    end
+
+    recipes
+  end
 end
