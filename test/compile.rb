79b7a10948c1fbf3990d0d4b033d193f44ea486f
Ross Andrews <randrews@geekfu.org>
2010-03-09 21:14:20 -0600

    Re-indent

--- test/compile.rb
+++ test/compile.rb
@@ -3,47 +3,47 @@ require "fileutils"
 
 describe "Compile" do
 
-  before :each do
-    @pwd = FileUtils.pwd
-    @scratch = File.join(File.dirname(__FILE__),"scratch")
-    FileUtils.mkdir @scratch
-    FileUtils.cd @scratch
-  end
-
-  after :each do
-    FileUtils.cd @pwd
-    FileUtils.rm_rf @scratch
-  end
-
-  it "should build a script" do
-    File.open("foo","w"){|f| f.print "Contents of the file" }
-    File.open("recipe","w"){|f|
-      f.puts "directory 'blah' do"
-      f.puts "  file 'file1', :from=>'foo'"
-      f.puts "end"
-    }
-
-    recipe = nil
-    lambda{recipe = Groundwork::Recipe.compile_file("recipe") }.should_not raise_error
-    recipe.should_not be_nil
-  end
-
-  it "should run a script correctly" do
-    File.open("foo","w"){|f| f.print "Contents of the file" }
-    File.open("recipe","w"){|f|
-      f.puts "directory 'blah' do"
-      f.puts "  file 'file1', :from=>'foo'"
-      f.puts "end"
-    }
-
-    recipe = Groundwork::Recipe.compile_file("recipe")
-    File.open("compiled","w"){|f| f.print recipe }
-
-    FileUtils.mkdir "tmp"
-    FileUtils.cd "tmp" do
-      lambda{ Groundwork::Recipe.run "../compiled" }.should_not raise_error
-      File.exists?("blah/file1").should be_true
-      File.read("blah/file1").should == "Contents of the file"
+    before :each do
+        @pwd = FileUtils.pwd
+        @scratch = File.join(File.dirname(__FILE__),"scratch")
+        FileUtils.mkdir @scratch
+        FileUtils.cd @scratch
     end
-  end
+
+    after :each do
+        FileUtils.cd @pwd
+        FileUtils.rm_rf @scratch
+    end
+
+    it "should build a script" do
+        File.open("foo","w"){|f| f.print "Contents of the file" }
+        File.open("recipe","w"){|f|
+            f.puts "directory 'blah' do"
+            f.puts "  file 'file1', :from=>'foo'"
+            f.puts "end"
+        }
+
+        recipe = nil
+        lambda{recipe = Groundwork::Recipe.compile_file("recipe") }.should_not raise_error
+        recipe.should_not be_nil
+    end
+
+    it "should run a script correctly" do
+        File.open("foo","w"){|f| f.print "Contents of the file" }
+        File.open("recipe","w"){|f|
+            f.puts "directory 'blah' do"
+            f.puts "  file 'file1', :from=>'foo'"
+            f.puts "end"
+        }
+
+        recipe = Groundwork::Recipe.compile_file("recipe")
+        File.open("compiled","w"){|f| f.print recipe }
+
+        FileUtils.mkdir "tmp"
+        FileUtils.cd "tmp" do
+            lambda{ Groundwork::Recipe.run "../compiled" }.should_not raise_error
+            File.exists?("blah/file1").should be_true
+            File.read("blah/file1").should == "Contents of the file"
+        end
+    end
 end
