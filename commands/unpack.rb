37368671146c59e391424606f954f2acf31345a6
Ross Andrews <randrews@geekfu.org>
2010-03-09 22:53:35 -0600

    Implemented unpack command

--- commands/unpack.rb
+++ commands/unpack.rb
@@ -1,16 +1,27 @@
 module Groundwork
-  module Commands
-    def self.unpack args
-      cmd_opts = Trollop::options(args) do
-        banner <<-STR
+    module Commands
+        def self.unpack args
+            cmd_opts = Trollop::options(args) do
+                opt :force, "Delete directory if exists", :default=>false
+                banner <<-STR
 Usage:
       groundwork unpack <name>
 
 Expands an installed recipe to the current directory
 STR
-      end
+            end
 
-      
+            raise RuntimeError.new("Recipe not found: #{args[0]}") unless Groundwork.known_recipes[args[0]]
+
+            if File.exists?(args[0])
+                if cmd_opts[:force]
+                    FileUtils.rm_rf args[0]
+                else
+                    raise RuntimeError.new("Directory already exists: #{args[0]}\nUse -f to replace")
+                end
+            end
+
+            Groundwork::Recipe.unpack args[0], Groundwork.known_recipes[args[0]]
+        end
     end
-  end
 end
